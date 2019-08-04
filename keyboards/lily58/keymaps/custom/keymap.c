9d026c001d2bb58b4a74f495d9b65454b13b3934
nijika-mbp <nijika.xyz@gmail.com>
2019-08-04 20:44:12 +0900

    modify QWERTY keymap

--- keyboards/lily58/keymaps/custom/keymap.c
+++ keyboards/lily58/keymaps/custom/keymap.c
@@ -28,8 +28,15 @@ enum custom_keycodes {
   LOWER,
   RAISE,
   ADJUST,
+  LCTLALT,
+  RCTLALT,
 };
 
+#define KC_LSSPC LSFT_T(KC_SPACE)
+#define KC_RSENT RSFT_T(KC_ENTER)
+#define KC_LCTAL LCTLALT
+#define KC_RCTAL RCTLALT
+#define KC_LSFES LSFT_T(KC_ESC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -49,11 +56,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QWERTY] = LAYOUT( \
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV, \
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
-  KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
-                             KC_LALT, KC_LGUI,LOWER, KC_SPC,   KC_ENT,   RAISE,   KC_BSPC, KC_RGUI \
+// ,-----------------------------------------------------------------.                     ,-----------------------------------------------------------------.
+         KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,                           KC_F7,     KC_F8,     KC_F9,    KC_F10,    KC_F11,    KC_F12, \
+// |----------+----------+----------+----------+----------+----------|                     |----------+----------+----------+----------+----------+----------|
+        KC_TAB,      KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,                            KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,   KC_BSPC, \
+// |----------+----------+----------+----------+----------+----------|                     |----------+----------+----------+----------+----------+----------|
+      KC_LCTRL,      KC_A,      KC_S,      KC_D,      KC_F,      KC_G,                            KC_H,      KC_J,      KC_K,      KC_L,   KC_SCLN,   KC_QUOT, \
+// |----------+----------+----------+----------+----------+----------|----------.----------|----------+----------+----------+----------+----------+----------|
+         LSFES,      KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,   KC_LBRC,   KC_RBRC,      KC_N,      KC_M,   KC_COMM,    KC_DOT,   KC_SLSH,   KC_MINS, \
+// `--------------------------------+----------+----------+----------|----------|----------|----------+----------+----------+--------------------------------'
+                                        KC_LALT,     LCTAL,     LOWER,     LSSPC,    RSENT,      RAISE,     RCTAL,   KC_RGUI \
+//                                   `------------------------------------------'-------------------------------------------'
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -230,6 +243,28 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           layer_on(_ADJUST);
         } else {
           layer_off(_ADJUST);
+        }
+        return false;
+        break;
+    case LCTLALT:
+        if (record->event.pressed) {
+          register_code(KC_LCTL);
+        } else {
+          unregister_code(KC_LCTL);
+          if (is_tapped) {
+            tap_code(KC_LALT);
+          }
+        }
+        return false;
+        break;
+    case RCTLALT:
+        if (record->event.pressed) {
+          register_code(KC_RCTL);
+        } else {
+          unregister_code(KC_RCTL);
+          if (is_tapped) {
+            tap_code(KC_RALT);
+          }
         }
         return false;
         break;
